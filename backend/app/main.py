0d2ba49f44f4959258fa07a69a8e7be4988cda52
Prarinam Deria <prarinamderia2000@gmail.com>
2025-09-26 00:11:09 +0530

    feat: user register with hashed_password done

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-
+from app.account.routers import router as account_router
 app = FastAPI(title="Ecommerce website")
 
 @app.get("/")
 def root():
     return f"I am root endpoint"
+
+app.include_router(account_router)
